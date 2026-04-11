0f3f076d94dcbd3b503b946929e966b81f68c3bf
julianmarta03 <julian.marta@bia.app>
2026-04-11 10:48:34 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,26 +1,18 @@
-const CACHE_NAME = 'inventario-bia-v2';
-const FILES = [
-  '/Inventari-bia/',
-  '/Inventari-bia/index.html',
-  '/Inventari-bia/manifest.json'
-];
+const CACHE_NAME = 'inventario-bia-v3';
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
-  );
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request))
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+      Promise.all(keys.map(k => caches.delete(k)))
     )
   );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
